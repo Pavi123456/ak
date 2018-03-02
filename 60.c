377c1cd4c06b19cd6f2c940029590df8a35b305e
franklinpirate <35331683+franklinpirate@users.noreply.github.com>
2018-03-02 16:04:40 +0530

    Update 60.c

--- 60.c
+++ 60.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-int a,b,temp,i,n;
-printf("enter the vaue",\n);
-
-scanf("%d%d",&a,&b);
+int f1,f2,f3,i,n;
+printf("enter the vaue:\n");
+scanf("%d",&n);
+scanf("%d%d",&f1,&f2);
+printf("%d\t%d\t",f1,f2);
 for(i=2;i<=n;i++)
-temp=a+b;
-a=b;
-b=temp;
-printf("the fibanocci number is%d",temp);
+f3=f1+f2;
+printf("%d\t",f3);
+f1=f2;
+f2=f3;
 return 0;
 }
