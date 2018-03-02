2f2b46ad328674b7973cdb54675744ab2cccbc4a
franklinpirate <35331683+franklinpirate@users.noreply.github.com>
2018-03-02 16:08:12 +0530

    Update 60.c

--- 60.c
+++ 60.c
@@ -2,14 +2,16 @@
 int main()
 {
 int f1,f2,f3,i,n;
-printf("enter the vaue:\n");
+printf("enter the n value:\n");
 scanf("%d",&n);
-scanf("%d%d",&f1,&f2);
+scanf("%d %d",&f1,&f2);
 printf("%d\t%d\t",f1,f2);
-for(i=2;i<=n;i++)
+for(i=2;i<n;i++)
+{
 f3=f1+f2;
 printf("%d\t",f3);
 f1=f2;
 f2=f3;
+}
 return 0;
 }
