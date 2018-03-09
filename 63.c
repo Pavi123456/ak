6e5f267cbdfec682799d7f1e99335339a14297e6
franklinpirate <35331683+franklinpirate@users.noreply.github.com>
2018-03-09 10:45:22 +0000

    Update 63.c

--- 63.c
+++ 63.c
@@ -3,13 +3,16 @@
 int main()
 {
 int i,count=0;
-char s[100];
-scanf("%d",&s);
-for(i=0;s[i]1='0';i++)
+char a[100];
+printf("enter:");
+scanf("%[^\n]s",a);
 {
-if(s[i]=='0')
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
 count++;
 }
 printf("the total number of word is%d",count+1);
+}
 return 0;
 }
